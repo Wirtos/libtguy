0df62a54d7b0ec84d72b9c64addf91336af1c515
Wirtos_new <Wirtos.new@gmail.com>
2021-01-18 21:14:30 +0200

    Allow building shared library and create export headers for it

--- include/libtguy.h
+++ include/libtguy.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <libtguy_export.h>
 
 typedef struct {
     const char *str;
@@ -11,15 +12,15 @@ typedef struct {
 
 typedef struct TrashGuyState TrashGuyState;
 
-TrashGuyState *tguy_init_arr(const CString arr[], size_t len, int starting_distance);
-TrashGuyState *tguy_init_str(const char *string, size_t len, int starting_distance);
-void tguy_free(TrashGuyState *st);
+LIBTGUY_EXPORT TrashGuyState *tguy_init_arr(const CString arr[], size_t len, int starting_distance);
+LIBTGUY_EXPORT TrashGuyState *tguy_init_str(const char *string, size_t len, int starting_distance);
+LIBTGUY_EXPORT void tguy_free(TrashGuyState *st);
 
-void tguy_from_frame(TrashGuyState *st, int frame);
-int tguy_get_frames_count(const TrashGuyState *st);
-size_t tguy_get_bsize(const TrashGuyState *st);
+LIBTGUY_EXPORT void tguy_from_frame(TrashGuyState *st, int frame);
+LIBTGUY_EXPORT int tguy_get_frames_count(const TrashGuyState *st);
+LIBTGUY_EXPORT size_t tguy_get_bsize(const TrashGuyState *st);
 
-void tguy_fprint(const TrashGuyState *st, FILE *fp);
-void tguy_print(const TrashGuyState *st);
-void tguy_bprint(const TrashGuyState * st, char *buf);
+LIBTGUY_EXPORT void tguy_fprint(const TrashGuyState *st, FILE *fp);
+LIBTGUY_EXPORT void tguy_print(const TrashGuyState *st);
+LIBTGUY_EXPORT void tguy_bprint(const TrashGuyState * st, char *buf);
 #endif //LIBTGUY_H
