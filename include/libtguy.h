8b43c972a2323d3c7632048c1c70cc6d99dfb00e
Wirtos_new <Wirtos.new@gmail.com>
2021-03-03 10:59:24 +0200

    final api change before utf8proc adoption

--- include/libtguy.h
+++ include/libtguy.h
@@ -6,7 +6,7 @@
 #include <libtguy_export.h>
 
 #define TGUY_VER_MAJOR 0
-#define TGUY_VER_MINOR 3
+#define TGUY_VER_MINOR 4
 #define TGUY_VER_PATCH 0
 
 typedef struct {
@@ -21,7 +21,7 @@ typedef struct {
 
 typedef struct TrashGuyState TrashGuyState;
 
-LIBTGUY_EXPORT TrashGuyState *tguy_from_arr_ext(const TGString *arr, size_t len, unsigned spacing,
+LIBTGUY_EXPORT TrashGuyState *tguy_from_arr_ex(const TGString *arr, size_t len, unsigned spacing,
         TGString sprite_space, TGString sprite_can, TGString sprite_left, TGString sprite_right);
 LIBTGUY_EXPORT TrashGuyState *tguy_from_arr(const TGString *arr, size_t len, unsigned spacing);
 LIBTGUY_EXPORT TrashGuyState *tguy_from_utf8(const char *string, size_t len, unsigned spacing);
@@ -34,5 +34,6 @@ LIBTGUY_EXPORT size_t tguy_get_bsize(TrashGuyState *st);
 LIBTGUY_EXPORT void tguy_fprint(const TrashGuyState *st, FILE *fp);
 LIBTGUY_EXPORT void tguy_print(const TrashGuyState *st);
 LIBTGUY_EXPORT void tguy_bprint(const TrashGuyState * st, char *buf);
+LIBTGUY_EXPORT const TGString *tguy_get_arr(const TrashGuyState *st, size_t *len);
 
-#endif // LIBTGUY_H
+#endif /* LIBTGUY_H */
--- src/libtguy.c
+++ src/libtguy.c
@@ -5,7 +5,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-
 typedef struct {
     TGString *data;
     size_t len;
@@ -23,7 +22,6 @@ struct TrashGuyState {
     unsigned cur_frame;
     unsigned max_frames;
     size_t bufsize;
-
 };
 
 /*
@@ -64,7 +62,8 @@ static inline void tguy_clear_field(TrashGuyState *st, unsigned n_erase_elements
     unsigned items_offset = st->initial_frames_count / 2 + n_erase_elements + 1;
     size_t flen = st->field.len - st->text.len + n_erase_elements;
     #ifdef TGUY_FASTCLEAR
-    memcpy(&st->field.data[0], &st->empty_field_.data[0], flen * sizeof(*st->field.data));
+    memcpy(&st->field.data[0], &st->empty_field_.data[0],
+        flen * sizeof(*st->field.data));
     #else
     for (size_t i = 1; i < flen; i++) {
         st->field.data[i] = st->sprite_space;
@@ -77,9 +76,10 @@ static inline void tguy_clear_field(TrashGuyState *st, unsigned n_erase_elements
 /*
  * initialize state from array of strings
  */
-TrashGuyState *tguy_from_arr_ext(const TGString *arr, size_t len, unsigned spacing,
+TrashGuyState *tguy_from_arr_ex(const TGString *arr, size_t len, unsigned spacing,
     TGString sprite_space, TGString sprite_can, TGString sprite_left, TGString sprite_right) {
     struct TrashGuyState *st;
+    assert(len < (unsigned) -1);
     st = malloc(sizeof(*st));
     if (st == NULL) goto fail;
     {
@@ -99,7 +99,7 @@ TrashGuyState *tguy_from_arr_ext(const TGString *arr, size_t len, unsigned spaci
         /* currently unused */
         st->cur_frame = (unsigned) -1;
         /* number of frames up to the last + 1 */
-        st->max_frames = get_frame_lower_boundary(st->initial_frames_count, st->text.len) + 1;
+        st->max_frames = get_frame_lower_boundary(st->initial_frames_count, (unsigned) st->text.len) + 1;
         st->data_ = NULL;
     }
     {
@@ -135,14 +135,13 @@ TrashGuyState *tguy_from_arr_ext(const TGString *arr, size_t len, unsigned spaci
 }
 
 TrashGuyState *tguy_from_arr(const TGString *arr, size_t len, unsigned spacing) {
-    return tguy_from_arr_ext(arr, len, spacing,
+    return tguy_from_arr_ex(arr, len, spacing,
         TGStringConst(" "),
         TGStringConst("\xf0\x9f\x97\x91"),
         TGStringConst("<(^_^ <)"),
         TGStringConst("(> ^_^)>")
     );
 }
-
 
 /*
  * initialize state from utf-8 string (each codepoint will be used as an element)
@@ -151,7 +150,7 @@ TrashGuyState *tguy_from_utf8(const char *string, size_t len, unsigned spacing) 
     TrashGuyState *st;
     TGString *strarr;
     size_t flen;
-    len = (len == ((size_t) -1)) ? strlen(string) : len;
+    len = (len == (size_t) -1) ? strlen(string) : len;
     flen = utf8_distance(&string[0], &string[len]);
     strarr = malloc(sizeof(*strarr) * flen);
     if (strarr == NULL) return NULL;
@@ -184,20 +183,20 @@ void tguy_free(TrashGuyState *st) {
  * Fill the state with desirable frame
  */
 void tguy_set_frame(TrashGuyState *st, unsigned frame) {
-    /* (n ^ 2) + (a1 - 1)n - frame = 0 */
+    /*         a                        b                              c       */
+    /* (element_index)^2 + (initial_frames_count - 1)element_index - frame = 0 */
     assert(frame >= 0 && frame < st->max_frames);
     if (st->cur_frame == frame) return;
     {
-        /* int a = 1,*/
+        /* unsigned a = 1,*/
         unsigned b = (st->initial_frames_count - 1),
             c = frame;
         /* school math */
-        unsigned n = ((unsigned) sqrt((b * b) + (c << 2)/* a */ ) - b) / 2 /* a */;
-        assert(n >= 0);
+        unsigned element_index = ((unsigned) sqrt((b * b) + (c << 2)/* a */ ) - b) / 2 /* a */;
         /* total number of frames drawn for moving the letter with index n (moving from the start to the end and backwards) */
-        unsigned frames_per_n = st->initial_frames_count + (2 * n);
+        unsigned frames_per_n = st->initial_frames_count + (2 * element_index);
         /* order of the frame in the frame series (up to frames_per_n) */
-        unsigned sub_frame = (frame - get_frame_lower_boundary(st->initial_frames_count, n));
+        unsigned sub_frame = (frame - get_frame_lower_boundary(st->initial_frames_count, element_index));
         /* if we're in the first half frames we're moving right, otherwise left */
         unsigned right = (sub_frame < frames_per_n / 2);
         /* index yields 0 twice, the difference is whether we're moving right */
@@ -205,18 +204,19 @@ void tguy_set_frame(TrashGuyState *st, unsigned frame) {
 
         st->cur_frame = frame; /* unused */
         /* if we're not moving right, then we're not drawing the element n because trashguy holds it */
-        tguy_clear_field(st, n + !right);
+        tguy_clear_field(st, element_index + !right);
 
         /* don't overwrite the trash can */
         st->field.data[i + 1] = right ? st->sprite_right : st->sprite_left;
         /* Draw the element trashguy holds */
         if (!right && i != 0) {
-            st->field.data[i] = st->text.data[n];
+            st->field.data[i] = st->text.data[element_index];
         }
     }
 }
 
 void tguy_fprint(const TrashGuyState *st, FILE *fp) {
+    assert(st->cur_frame != (unsigned) -1);
     for (size_t i = 0, flen = st->field.len; i < flen; i++) {
         fprintf(fp, "%.*s", (int) st->field.data[i].len, st->field.data[i].str);
     }
@@ -225,6 +225,7 @@ void tguy_fprint(const TrashGuyState *st, FILE *fp) {
 void tguy_print(const TrashGuyState *st) { tguy_fprint(st, stdout); }
 
 void tguy_bprint(const TrashGuyState *st, char *buf) {
+    assert(st->cur_frame != (unsigned) -1);
     for (size_t i = 0, flen = st->field.len; i < flen; i++) {
         const char *s = st->field.data[i].str;
         for (size_t j = 0, slen = st->field.data[i].len; j < slen; j++) {
@@ -234,24 +235,29 @@ void tguy_bprint(const TrashGuyState *st, char *buf) {
     *buf = '\0';
 }
 
+const TGString *tguy_get_arr(const TrashGuyState *st, size_t *len) {
+    assert(st->cur_frame != (unsigned) -1);
+    *len = st->field.len;
+    return st->field.data;
+}
+
 unsigned tguy_get_frames_count(const TrashGuyState *st) {
     return st->max_frames;
 }
-#ifndef max
-    #define max(a, b) ((a) > (b) ? (a) : (b))
-#endif
+
+#define tg_max(a, b) ((a) > (b) ? (a) : (b))
 
 size_t tguy_get_bsize(TrashGuyState *st) {
     /* nul terminator */
     size_t sz = 1;
-    if (st->bufsize){
+    if (st->bufsize) {
         return st->bufsize;
     }
     /* overall text length */
     for (size_t i = 0, tlen = st->text.len; i < tlen; i++) {
         /* text letter will be replaced with space (filler sprite) eventually
          * by choosing the largest ensure the buffer is large enough */
-        sz += max(st->text.data[i].len, st->sprite_space.len);
+        sz += tg_max(st->text.data[i].len, st->sprite_space.len);
     }
     /* overall free space length */
     for (size_t i = 0, slen = (st->initial_frames_count / 2) - 1; i < slen; i++) {
@@ -259,8 +265,10 @@ size_t tguy_get_bsize(TrashGuyState *st) {
     }
     /* singleton sprites lengths */
     sz += st->sprite_can.len;
-    sz += max(st->sprite_right.len, st->sprite_left.len);
+    sz += tg_max(st->sprite_right.len, st->sprite_left.len);
 
     st->bufsize = sz;
     return sz;
 }
+
+#undef tg_max
