6c8bd8471c6c5401ca0c61aa5b9c8bf62dfd5fa7
Wirtos_new <Wirtos.new@gmail.com>
2021-08-01 00:12:20 +0300

    Update GetVersionFromMacro.cmake

--- cmake/GetVersionFromMacro.cmake
+++ cmake/GetVersionFromMacro.cmake
@@ -1,41 +1,35 @@
 function(get_version_from_macro)
+    set(options)
     set(oneValueArgs OUTPUT PREFIX HEADER)
     set(multiValueArgs VERSIONS)
-    cmake_parse_arguments(
-            "LOCAL"
-            ""
+    cmake_parse_arguments(_args
+            "${options}"
             "${oneValueArgs}"
             "${multiValueArgs}"
             ${ARGN})
 
-    if(NOT DEFINED LOCAL_OUTPUT)
-        message(FATAL_ERROR "OUTPUT not defined")
-    endif()
+    foreach (arg IN ITEMS OUTPUT PREFIX HEADER)
+        if (NOT DEFINED _args_${arg})
+            message(FATAL_ERROR "${arg} not provided")
+        endif ()
+    endforeach ()
 
-    if (NOT DEFINED LOCAL_PREFIX)
-        message(FATAL_ERROR "PREFIX not defined")
-    endif ()
-
-    if (NOT DEFINED LOCAL_HEADER)
-        message(FATAL_ERROR "HEADER not defined")
-    endif ()
-
-    if (NOT DEFINED LOCAL_VERSIONS)
+    if (NOT DEFINED _args_VERSIONS)
         message(STATUS "VERSIONS not provided, using default: MAJOR MINOR PATCH")
-        set(LOCAL_VERSIONS MAJOR MINOR PATCH)
+        set(_args_VERSIONS MAJOR MINOR PATCH)
     endif ()
 
     set(HEADER_VER)
-    foreach(version IN ITEMS ${LOCAL_VERSIONS})
-        set(macrostr "#define ${LOCAL_PREFIX}${version}")
-        file(STRINGS "${LOCAL_HEADER}" version REGEX "${macrostr}")
+    foreach (version IN ITEMS ${_args_VERSIONS})
+        set(macrostr "#define ${_args_PREFIX}${version}")
+        file(STRINGS "${_args_HEADER}" version REGEX "${macrostr}")
         string(REGEX REPLACE "${macrostr}[ \\t]+([0-9]+)" "" version "${version}")
         if ("${CMAKE_MATCH_1}" STREQUAL "")
             message(FATAL_ERROR "macro \"${macrostr} <num>\" not found")
         endif ()
         list(APPEND HEADER_VER "${CMAKE_MATCH_1}")
-    endforeach()
+    endforeach ()
 
     string(JOIN "." HEADER_VER ${HEADER_VER})
-    set(${LOCAL_OUTPUT} ${HEADER_VER} PARENT_SCOPE)
+    set(${_args_OUTPUT} ${HEADER_VER} PARENT_SCOPE)
 endfunction()
