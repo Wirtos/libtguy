94e384bb90ea66fbe8de63f62d73b60bb6d18dbb
Wirtos <Wirtos@users.noreply.github.com>
2025-12-04 20:10:46 +0000

    deploy: 8c38c5e4d8cf6d32700afca575d5c8d58ee8baea

--- search/all_6.js
+++ search/all_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['len_0',['len',['../structTGStrView.html#a9199bd0666753a8f274450bc26b7a9ad',1,'TGStrView::len'],['../structTGStrViewArr.html#a0c29c299af38dc6877d4c28e3e421e48',1,'TGStrViewArr::len']]],
+  ['len_0',['len',['../structTGStrViewArr.html#a0c29c299af38dc6877d4c28e3e421e48',1,'TGStrViewArr::len'],['../structTGStrView.html#a9199bd0666753a8f274450bc26b7a9ad',1,'TGStrView::len']]],
   ['libtguy_2ec_1',['libtguy.c',['../libtguy_8c.html',1,'']]],
   ['libtguy_2eh_2',['libtguy.h',['../libtguy_8h.html',1,'']]]
 ];
